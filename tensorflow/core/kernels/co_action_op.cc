79da4bc8b1530807baad232602eac9bc0a8b7986
marvinYu <weifei.yu@intel.com>
2022-08-04 22:14:15 +0800

    [UT] add co_action op test.

--- tensorflow/core/kernels/co_action_op.cc
+++ tensorflow/core/kernels/co_action_op.cc
@@ -21,6 +21,34 @@
 
 namespace tensorflow {
 
+static std::chrono::high_resolution_clock::time_point _start = 
+    std::chrono::high_resolution_clock::now();
+template <typename T>
+void ShowLog(const T& msg) {
+  auto _now = std::chrono::high_resolution_clock::now();
+
+  std::cout << ">>>>>>>>> marvin test <<<<<<<<<" << std::endl
+            << ">>>- time = "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(_now - _start).count()
+            << " ms; " << std::endl
+            << ">>>-  msg = " << msg << std::endl
+            << ">>>-------------------------<<<" << std::endl;
+  _start = _now;
+}
+
+template <typename T>
+void ShowLog(
+  const std::chrono::high_resolution_clock::time_point start,
+  const std::chrono::high_resolution_clock::time_point end,
+  const T& msg) {
+  std::cout << ">>>>>>>>> marvin test <<<<<<<<<" << std::endl
+            << " - time = "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+            << " ms; " << std::endl
+            << " -  msg = " << msg << std::endl
+            << "-------------------------------" << std::endl;
+}
+
 template <typename T>
 Status PowCPU(const CPUDevice& d, size_t m, size_t n, const T* in, T* out,
               int64 pow_num) {
@@ -217,6 +245,252 @@ class CoActionOp : public OpKernel {
   int64 pow_num;
 };
 
+//----------------------------------------------------------------------------//
+// Optimize code are below.                                                   //
+//----------------------------------------------------------------------------//
+#define INDEX(x, y, ld) ((x) * (ld) + (y))
+#define ADDRESS(p, x, y, ld) ((p) + (x) * (ld) + (y))
+
+template <typename T>
+Status OptPowCPU(const CPUDevice& d, size_t m, size_t n, const T* in, T* out,
+              int64 pow_num) {
+  typename tensorflow::TTypes<const T>::Matrix in_matrix(in, m, n);
+  typename tensorflow::TTypes<T>::Matrix out_matrix(out, m, n);
+  out_matrix.device(d) = in_matrix.pow((T)pow_num);
+  return Status::OK();
+}
+
+template <typename T>
+Status OptCoActionCPU(const CPUDevice& d, size_t m, size_t n, size_t k, const T* a,
+                   const T* b, T* c) {
+  typename tensorflow::TTypes<const T>::Matrix a_matrix(a, m, k);
+  typename tensorflow::TTypes<const T>::Matrix b_matrix(b, k, n);
+  typename tensorflow::TTypes<T>::Matrix c_matrix(c, 1, n);
+  Eigen::array<Eigen::IndexPair<Eigen::DenseIndex>, 1> dim_pair;
+  dim_pair[0].first = 1;
+  dim_pair[0].second = 0;
+  Eigen::array<int, 1> reduce_dims({0});
+  c_matrix.device(d) =
+      a_matrix.contract(b_matrix, dim_pair).tanh().sum(reduce_dims);
+  return Status::OK();
+}
+
+template <typename Scalar>
+struct LaunchOptCoAction<CPUDevice, Scalar> {
+  Status operator()(OpKernelContext* context, int64 m, int64 n, int64 k,
+                    const Tensor& in_a, const Tensor& in_b, Tensor* out,
+                    int64 batch_a, int64 batch_b, int64 paralle_num,
+                    int64 pow_num) {
+    auto a_ptr = in_a.template flat<Scalar>().data();
+    auto b_ptr = in_b.template flat<Scalar>().data();
+    auto c_ptr = out->template flat<Scalar>().data();
+    Tensor tmp_pow;
+    TF_RETURN_IF_ERROR(context->allocate_temp(
+        DataTypeToEnum<Scalar>::value,
+        TensorShape({batch_a, paralle_num, pow_num * m, k}), &tmp_pow));
+    auto tmp_pow_ptr = tmp_pow.template flat<Scalar>().data();
+    // power and concat
+#ifdef __AVX512F__
+    const int total_element = m * k;
+    const int total_loops = total_element / 16;
+    const int remain = total_element % 16;
+    const __mmask16 row_mask =
+        (static_cast<std::uint32_t>(1) << remain) - 1;
+
+    __m512 element_row;
+    for (int64 batch = 0; batch < batch_a; batch++) {
+      for (int64 p = 0; p < paralle_num; p++) {
+        int loop = 0;
+        for (; loop < total_loops; loop++){
+          element_row = _mm512_loadu_ps(a_ptr + (batch * paralle_num + p) * m * k + 16 * loop);
+          _mm512_storeu_ps( // pow(1)
+            tmp_pow_ptr + (batch * paralle_num + p) * pow_num * m * k + 16 * loop, element_row);
+          _mm512_storeu_ps( // pow(2)
+            tmp_pow_ptr + (batch * paralle_num + p) * pow_num * m * k + m * k + 16 * loop,
+            _mm512_mul_ps(element_row, element_row));
+        }
+        if (remain){
+          element_row = _mm512_maskz_loadu_ps(row_mask, a_ptr + (batch * paralle_num + p) * m * k + 16 * loop);
+          _mm512_mask_storeu_ps( // pow(1)
+            tmp_pow_ptr + (batch * paralle_num + p) * pow_num * m * k + 16 * loop, row_mask, element_row);
+          _mm512_mask_storeu_ps( // pow(2)
+            tmp_pow_ptr + (batch * paralle_num + p) * pow_num * m * k + m * k + 16 * loop, row_mask,
+            _mm512_mul_ps(element_row, element_row));
+        }
+      }
+    }
+#else
+    for (int64 batch = 0; batch < batch_a; batch++) {
+      for (int64 p = 0; p < paralle_num; p++) {
+        for (int64 pow = 0; pow < pow_num; pow++) {
+          TF_RETURN_IF_ERROR(OptPowCPU<Scalar>(
+              context->eigen_device<CPUDevice>(), m, k,
+              a_ptr + (batch * paralle_num + p) * m * k,
+              tmp_pow_ptr + (batch * paralle_num + p) * pow_num * m * k +
+                  pow * m * k,
+              pow + 1));
+        }
+      }
+    }
+#endif
+
+    // MatMul + tanh + sum parts
+#ifdef __AVX512F__
+    // ShowLog("OptCoActionOp:LaunchCoAction:start DNN");
+
+    // float* temp_c = new float[2 * m];
+    // float* temp_b = new float[k];
+    // const int total_element = m * k;
+    // const int total_loops = total_element / 16;
+    // const int remain = total_element % 16;
+    // const __mmask16 row_mask =
+    //     (static_cast<std::uint32_t>(1) << remain) - 1;
+
+    // __m512 element_row;
+    // __m512 element_col[n];
+
+    // for (int64 batch = 0; batch < batch_b; batch++) {
+    //   for (int64 p = 0; p < paralle_num; p++) {
+    //     // 1. init B
+    //     for(int iter; iter < n; iter++){
+    //       float* _tmp_b_ptr = b_ptr + (batch * paralle_num + p) * k * n;
+    //       // hard code here, cause of the k is fixed as 5.
+    //       element_col[iter] = _mm512_set_ps(
+    //         /* 15 */0,
+    //         /* 14 */ADDRESS(_tmp_b_ptr, 4, 0, n),
+    //         /* 13 */ADDRESS(_tmp_b_ptr, 3, 0, n),
+    //         /* 12 */ADDRESS(_tmp_b_ptr, 2, 0, n),
+    //         /* 11 */ADDRESS(_tmp_b_ptr, 1, 0, n),
+    //         /* 10 */ADDRESS(_tmp_b_ptr, 0, 0, n),
+    //         /*  9 */ADDRESS(_tmp_b_ptr, 4, 0, n),
+    //         /*  8 */ADDRESS(_tmp_b_ptr, 3, 0, n),
+    //         /*  7 */ADDRESS(_tmp_b_ptr, 2, 0, n),
+    //         /*  6 */ADDRESS(_tmp_b_ptr, 1, 0, n),
+    //         /*  5 */ADDRESS(_tmp_b_ptr, 0, 0, n),
+    //         /*  4 */ADDRESS(_tmp_b_ptr, 4, 0, n),
+    //         /*  3 */ADDRESS(_tmp_b_ptr, 3, 0, n),
+    //         /*  2 */ADDRESS(_tmp_b_ptr, 2, 0, n),
+    //         /*  1 */ADDRESS(_tmp_b_ptr, 1, 0, n),
+    //         /*  0 */ADDRESS(_tmp_b_ptr, 0, 0, n),
+    //       )
+    //     }
+    //     // 2. load part of A
+    //     for(int iter; iter < n; iter++){
+
+    //       element_col[iter]
+    //     }
+
+        
+    //   }
+    // }
+    for (int64 batch = 0; batch < batch_b; batch++) {
+      for (int64 p = 0; p < paralle_num; p++) {
+        for (int64 pow = 0; pow < pow_num; pow++) {
+          TF_RETURN_IF_ERROR(OptCoActionCPU<Scalar>(
+              context->eigen_device<CPUDevice>(), m, n, k,
+              tmp_pow_ptr + (p * pow_num + pow) * m * k,
+              b_ptr + (batch * paralle_num + p) * k * n,
+              c_ptr + ((batch * paralle_num + p) * pow_num + pow) * 1 * n));
+        }
+      }
+    }
+    // ShowLog("OptCoActionOp:LaunchCoAction:end DNN");
+#else
+    for (int64 batch = 0; batch < batch_b; batch++) {
+      for (int64 p = 0; p < paralle_num; p++) {
+        for (int64 pow = 0; pow < pow_num; pow++) {
+          TF_RETURN_IF_ERROR(OptCoActionCPU<Scalar>(
+              context->eigen_device<CPUDevice>(), m, n, k,
+              tmp_pow_ptr + (p * pow_num + pow) * m * k,
+              b_ptr + (batch * paralle_num + p) * k * n,
+              c_ptr + ((batch * paralle_num + p) * pow_num + pow) * 1 * n));
+        }
+      }
+    }
+#endif
+    return Status::OK();
+  }
+};
+
+template <typename Device, typename Scalar>
+class OptCoActionOp : public OpKernel {
+ public:
+  explicit OptCoActionOp(OpKernelConstruction* context) : OpKernel(context) {
+    OP_REQUIRES_OK(context, context->GetAttr("pow_num", &pow_num));
+  }
+
+  ~OptCoActionOp() = default;
+
+  void Compute(OpKernelContext* ctx) override {
+    auto& a = ctx->input(0);
+    auto& b = ctx->input(1);
+
+    OP_REQUIRES(ctx, a.dims() == 4,
+                errors::InvalidArgument("In[0] ndims must be 4: ", a.dims()));
+    OP_REQUIRES(ctx, b.dims() == 4,
+                errors::InvalidArgument("In[1] ndims must be 4: ", b.dims()));
+    // currently only support m=150/50, k=5, n=4, pow=2
+    OP_REQUIRES(ctx, pow_num == 2,
+                errors::InvalidArgument("pow_num must == 2: ", pow_num));
+    OP_REQUIRES(
+        ctx, a.dim_size(2) == 50 || a.dim_size(2) == 150,
+        errors::InvalidArgument("m must be 50 or 150: ", a.dim_size(2)));
+    OP_REQUIRES(ctx, b.dim_size(2) == 5,
+                errors::InvalidArgument("k must be 5: ", b.dim_size(2)));
+    OP_REQUIRES(ctx, b.dim_size(3) == 4,
+                errors::InvalidArgument("n must be 4: ", b.dim_size(3)));
+
+    int64 d0 = a.dim_size(2);
+    int64 d1 = a.dim_size(3);
+    int64 d2 = b.dim_size(2);
+    int64 d3 = b.dim_size(3);
+    OP_REQUIRES(ctx, d1 == d2,
+                errors::InvalidArgument("a mismatch b shape: ", d1, " vs. ", d2,
+                                        ": ", a.shape().DebugString(), " ",
+                                        b.shape().DebugString()));
+    int64 batch_a = a.dim_size(0);
+    int64 batch_b = b.dim_size(0);
+    OP_REQUIRES(ctx, batch_a == 1,
+                errors::InvalidArgument("batch_a must be 1: a_shape = ",
+                                        a.shape().DebugString()));
+    int64 parallel_a = a.dim_size(1);
+    int64 parallel_b = b.dim_size(1);
+    OP_REQUIRES(ctx, parallel_a == parallel_b,
+                errors::InvalidArgument(
+                    "parallel_a mismatch parallel_b : ", parallel_a, " vs. ",
+                    parallel_b, ": ", a.shape().DebugString(), " ",
+                    b.shape().DebugString()));
+    TensorShape out_shape({batch_b, parallel_a, pow_num, d3});
+    Tensor* out = nullptr;
+    OP_REQUIRES_OK(ctx, ctx->allocate_output(0, out_shape, &out));
+    if (out->NumElements() == 0) {
+      return;
+    }
+    if (a.NumElements() == 0 || b.NumElements() == 0) {
+      functor::SetZeroFunctor<Device, Scalar> f;
+      f(ctx->eigen_device<Device>(), out->flat<Scalar>());
+      return;
+    }
+
+    //[PROF-STATS]
+    int64 delta = 2 * d1 * out_shape.num_elements() * pow_num;
+    if (VLOG_IS_ON(1)) {
+      LOG(INFO) << "FLOPs = " << delta
+                << ", " << type_string()
+                << ", " << name()
+                << ", " << a.shape().DebugString()
+                << ", " << b.shape().DebugString();
+    }
+
+    OP_REQUIRES_OK(ctx, LaunchOptCoAction<Device, Scalar>()(ctx, d0, d3, d1, a, b,
+                                                         out, batch_a, batch_b,
+                                                         parallel_a, pow_num));
+  }
+
+ private:
+  int64 pow_num;
+};
+
 template <typename Device, typename Scalar, typename TIndex>
 class CoActionIndicatorOp : public OpKernel {
  public:
@@ -307,6 +581,9 @@ class CoActionIndicatorOp : public OpKernel {
   REGISTER_KERNEL_BUILDER(                                              \
       Name("CoAction").Device(DEVICE_CPU).TypeConstraint<TYPE>("T"),    \
       CoActionOp<CPUDevice, TYPE>);                                     \
+  REGISTER_KERNEL_BUILDER(                                              \
+      Name("OptCoAction").Device(DEVICE_CPU).TypeConstraint<TYPE>("T"),\
+      OptCoActionOp<CPUDevice, TYPE>);                                  \
   REGISTER_KERNEL_BUILDER(Name("CoActionIndicator")                     \
                               .Device(DEVICE_CPU)                       \
                               .TypeConstraint<TYPE>("T")                \
--- tensorflow/core/kernels/co_action_op_test.cc
+++ tensorflow/core/kernels/co_action_op_test.cc
@@ -24,7 +24,164 @@ limitations under the License.
 #include "tensorflow/core/protobuf/rewriter_config.pb.h"
 #include "tensorflow/core/public/session.h"
 
+#include "tensorflow/core/framework/fake_input.h"
+#include "tensorflow/core/framework/node_def_builder.h"
+#include "tensorflow/core/framework/types.pb.h"
+
 namespace tensorflow {
+
+template <typename T>
+void show_log(T msg){
+  std::cout << std::endl << "------ marvin ------" << std::endl
+            << ">>> " << msg << std::endl
+            << "--------------------" << std::endl << std::endl;
+}
+//----------------------------------------------------------------------------//
+// Co-action Unit Tests are below.                                               //
+//----------------------------------------------------------------------------//
+using GraphRunner =
+    std::function<void(const Tensor& input_data, const Tensor& filter_data, Tensor* out)>;
+
+template <typename T>
+class CommonTestUtilities : public OpsTestBase {
+ public:
+  void PerformConversion(DataType dtype, const Tensor& tensor, Tensor* output) { // Default, convert shape
+    TF_EXPECT_OK(InitOp());
+    AddInputFromArray<T>(tensor.shape(), tensor.flat<T>());
+    TF_ASSERT_OK(RunOpKernel());
+
+    *output = *GetOutput(0);
+  }
+
+  // Runs a Tensorflow graph defined by the root scope, and fetches the result
+  // of 'fetch' node into the output Tensor.
+  static void RunAndFetch(const tensorflow::Scope& root, const string& fetch,
+                          Tensor* output, const NodeDef* fetch_node = nullptr) {
+    tensorflow::GraphDef graph;
+    TF_ASSERT_OK(root.ToGraphDef(&graph));
+
+    if (fetch_node) {
+      *graph.add_node() = *fetch_node;
+    }
+
+    std::unique_ptr<tensorflow::Session> session(
+        tensorflow::NewSession(tensorflow::SessionOptions()));
+    TF_ASSERT_OK(session->Create(graph));
+
+    std::vector<Tensor> unfused_tensors;
+    TF_ASSERT_OK(session->Run({}, {fetch}, {}, &unfused_tensors));
+
+    *output = unfused_tensors[0];
+  }
+
+  void TestBody() {}
+
+  // Compare two outcomes default & opt by calling run_default() & run_opt()
+  static void VerifyTensorClose(int bs, int pl, int m, int k, int n,
+                                     const GraphRunner& run_default,
+                                     const GraphRunner& run_opt) { 
+    DataType dtype = DataTypeToEnum<T>::v();
+
+    Tensor input(dtype, TensorShape({1, pl, m, k}));
+    input.flat<T>() = input.flat<T>().template setRandom<random_gen_>();
+
+    Tensor weight(dtype, TensorShape({bs, pl, k, n}));
+    weight.flat<T>() = weight.flat<T>().template setRandom<random_gen_>();
+
+    Tensor output;
+    Tensor opt_output;
+
+    run_default(input, weight, &output);
+    run_opt(input, weight, &opt_output);
+
+    ASSERT_EQ(output.dtype(), opt_output.dtype());
+    ASSERT_EQ(output.shape(), opt_output.shape());
+
+    test::ExpectClose(output, opt_output, 1e-5);
+  }
+
+ private:
+  using random_gen_ = Eigen::internal::NormalRandomGenerator<T>;
+};
+
+// Testing OptCoActionOpTest
+template <typename T>
+class OptCoActionOpTest : public OpsTestBase {
+ private:
+  void RunCoActionOp(const Tensor& input, const Tensor& weight, Tensor* output) {
+    Scope root = tensorflow::Scope::NewRootScope();
+
+    DataType dtype = DataTypeToEnum<T>::v();
+
+    Output o_input =
+        ops::Const(root.WithOpName("input"), Input::Initializer(input));
+    Output o_weight =
+        ops::Const(root.WithOpName("weight"), Input::Initializer(weight));
+
+    NodeDef co_action;
+    TF_EXPECT_OK(NodeDefBuilder("co_action", "CoAction") //build node
+                  .Input({o_input.name(), 0, dtype})
+                  .Input({o_weight.name(), 0, dtype})
+                  .Attr("pow_num", 2)
+                  .Finalize(&co_action));
+    
+    CommonTestUtilities<T>::RunAndFetch(root, co_action.name(), output, &co_action);
+  }
+
+  void RunOptCoActionOp(const Tensor& input, const Tensor& weight, Tensor* output) {
+    Scope root = tensorflow::Scope::NewRootScope();
+
+    DataType dtype = DataTypeToEnum<T>::v();
+
+    Output o_input =
+        ops::Const(root.WithOpName("input"), Input::Initializer(input));
+    Output o_weight =
+        ops::Const(root.WithOpName("weight"), Input::Initializer(weight));
+
+    NodeDef opt_co_action;
+    TF_EXPECT_OK(NodeDefBuilder("opt_co_action", "OptCoAction") //build node
+                  .Input({o_input.name(), 0, dtype})
+                  .Input({o_weight.name(), 0, dtype})
+                  .Attr("pow_num", 2)
+                  .Finalize(&opt_co_action));
+    
+    CommonTestUtilities<T>::RunAndFetch(root, opt_co_action.name(), output, &opt_co_action);
+  }
+
+ protected:
+  void VerifyOptCoAction(int bs, int pl, int m, int k, int n){
+    const GraphRunner run_default =
+        [this](const Tensor& input, const Tensor& weight, Tensor* output) {
+          RunCoActionOp(input, weight, output);
+        };
+
+    const GraphRunner run_opt =
+        [this](const Tensor& input, const Tensor& weight, Tensor* output) {
+          RunOptCoActionOp(input, weight, output);
+        };
+
+    CommonTestUtilities<T>::VerifyTensorClose(bs, pl, m, k, n, run_default, run_opt);
+  }
+};
+
+TYPED_TEST_CASE_P(OptCoActionOpTest);
+
+#define REGISTER_TEST_CASE(BS, PL, M, K, N)                                      \
+  TYPED_TEST_P(OptCoActionOpTest, OptCoAction##_##BS##_##PL##_##M##_##K##_##N) { \
+    this->VerifyOptCoAction(BS, PL, M, K, N);                                    \
+  }
+
+// REGISTER_TEST_CASE(200, 4, 50, 5, 4);
+REGISTER_TEST_CASE(200, 30, 150, 5, 4);
+
+REGISTER_TYPED_TEST_CASE_P(OptCoActionOpTest,
+                          // OptCoAction_200_4_50_5_4,
+                          OptCoAction_200_30_150_5_4
+                          );
+
+using OptCoActionDataTypes = ::testing::Types<float>;
+INSTANTIATE_TYPED_TEST_CASE_P(Test, OptCoActionOpTest,
+                              OptCoActionDataTypes);
 
 //----------------------------------------------------------------------------//
 // Performance benchmarks are below.                                          //
@@ -35,11 +192,11 @@ static Graph* Coaction(const string& kind, int m, int k, int n, int bs, int pl) 
   DataType type = DataTypeToEnum<T>::v();
 
   const bool isDefault = (kind == "Default");
-  string op_name = isDefault ? "CoAction" : "_OptCoAction";
+  string op_name = isDefault ? "CoAction" : "OptCoAction";
 
-  Tensor in0(type, TensorShape({bs, pl, m, k}));
+  Tensor in0(type, TensorShape({1, pl, m, k}));
   in0.flat<T>().setRandom();
-  Tensor in1(type, TensorShape({bs, pl, k, n}));
+  Tensor in1(type, TensorShape({200, pl, k, n}));
   in1.flat<T>().setRandom();
 
   Node* input_in0 = test::graph::Constant(g, in0);
@@ -59,7 +216,7 @@ static Graph* Coaction(const string& kind, int m, int k, int n, int bs, int pl) 
   static void BM_Coaction##_##kind##_##M##_##K##_##N##_##BS##_##PL##_##T##_##DEVICE##_##NTH( \
       int iters) {                                                                           \
     testing::UseRealTime();                                                                  \
-    testing::ItemsProcessed(static_cast<int64>(iters) * BS * PL * M * K * N * 2 * 2);        \
+    testing::ItemsProcessed(static_cast<int64>(iters) * 200 * PL * M * K * N * 2 * 2);       \
     SessionOptions opts;                                                                     \
     opts.config.set_intra_op_parallelism_threads(NTH);                                       \
     test::Benchmark(#DEVICE, Coaction<T>(#kind, M, K, N, BS, PL), &opts).Run(iters);         \
@@ -68,21 +225,20 @@ static Graph* Coaction(const string& kind, int m, int k, int n, int bs, int pl) 
 
 #define BM_Coaction_kind(M, K, N, BS, PL, T, DEVICE, NTH)     \
   BM_Coaction_Base(Default, M, K, N, BS, PL, T, DEVICE, NTH); \
-  // BM_Coaction_Base(Opt, M, K, N, BS, PL, T, DEVICE, NTH);  \
+  BM_Coaction_Base(Opt, M, K, N, BS, PL, T, DEVICE, NTH);     \
 
-#define BM_Coaction_NTH(M, K, N, BS, PL, T, DEVICE) \
-  BM_Coaction_kind(M, K, N, BS, PL, T, DEVICE, 1);  \
-  BM_Coaction_kind(M, K, N, BS, PL, T, DEVICE, 4);  \
-  BM_Coaction_kind(M, K, N, BS, PL, T, DEVICE, 8);  \
+#define BM_Coaction_NTH(M, K, N, BS, PL, T, DEVICE)    \
+  BM_Coaction_kind(M, K, N, BS, PL, T, DEVICE, 1);     \
+  // BM_Coaction_kind(M, K, N, BS, PL, T, DEVICE, 4);  \
+  // BM_Coaction_kind(M, K, N, BS, PL, T, DEVICE, 8);  \
 
-#define BM_Coaction(M, K, N, BS, PL)               \
-  BM_Coaction_NTH(M, K, N, BS, PL, float, cpu);    \
+#define BM_Coaction(M, K, N, BS, PL)                  \
+  BM_Coaction_NTH(M, K, N, BS, PL, float, cpu);       \
   // BM_Coaction_NTH(M, K, N, BS, PL, bfloat16, cpu); \
 
-// Vector * Vector
-BM_Coaction(50, 5, 4, 1, 1);
-BM_Coaction(50, 5, 4, 166, 1);
-BM_Coaction(150, 5, 4, 1, 1);
-BM_Coaction(150, 5, 4, 166, 1);
+// BM_Coaction(50, 5, 4, 1, 4);
+// BM_Coaction(50, 5, 4, 1, 30);
+// BM_Coaction(150, 5, 4, 1, 4);
+BM_Coaction(150, 5, 4, 1, 30);
 
 }  // end namespace tensorflow
